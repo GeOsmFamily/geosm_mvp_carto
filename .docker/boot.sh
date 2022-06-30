7729d981f7c8c3fdda98577960c8c50878c04f96
BorisGautier <tchoukouahaboris@outlook.fr>
2022-06-30 17:26:19 +0100

    add docker

---
+++ .docker/boot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+host=$(/sbin/ip route|awk '/default/ { print $3 }')
+echo ${host}
+
+a2dissite 000-default.conf 
+a2ensite service.conf
+service apache2 reload
+
+chown www-data:www-data /var/www/html/
+chmod -R 777 /var/www/html/
+
+cd /var/www/html/ && forever start  -a --minUptime 5000  --spinSleepTime 5000 -l process.log -o out.log -e err.log index.js
