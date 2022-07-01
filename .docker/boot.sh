4d1c9a1d1cbaee239bb0ed3d6b3e307f399a9c2b
BorisGautier <tchoukouahaboris@outlook.fr>
2022-07-01 12:35:31 +0100

    update project

--- .docker/boot.sh
+++ .docker/boot.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 host=$(/sbin/ip route|awk '/default/ { print $3 }')
 echo ${host}
+sed -i 's/localhost/'${host}'/g' "/home/root/carto/env.json"
 
 a2dissite 000-default.conf 
 a2ensite service.conf
 service apache2 reload
 
-chown www-data:www-data /var/www/html/
+chown www-data:www-data /var/www/
 chown www-data:www-data /home/root/
-chmod -R 777 /var/www/html/
+chmod -R 777 /var/www/
 chmod -R 777 /home/root/
 
 cd /home/root/carto/ && forever start  -a --minUptime 5000  --spinSleepTime 5000 -l process.log -o out.log -e err.log index.js
